2d906909612c0d00fd13cabf4f83fd62953e7c95
Alan Jackson <big.al.jackson9092@gmail.com>
2015-08-01 19:24:53 -0400

    Homes
    les homeses

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-var box = document.getElementById("search box"); 
+var box = document.getElementById("search_box"); 
 // this should catch most URLs, or at least the ones I would type.
 var urlPattern = new RegExp("^(https?://)?[^ ]+[.][^ ]+([.][^ ]+)*(/[^ ]+)?$");
 // search for text in text box
+function chgR(){
+	var searchdiv =document.getElementsByClassName("searchdiv")[0];
+	searchdiv.style["boxShadow"] = ("0px 0px 10px 2px red");
+	document.getElementById('hr1') .style["boxShadow"] = ("0px 0px 8px 0.75px red");
+	document.getElementById('hr2') .style["boxShadow"] = ("0px 0px 8px 0.75px red");
+}
+function chgW(){
+	var searchdiv =document.getElementsByClassName("searchdiv")[0];
+	searchdiv.style["boxShadow"] = ("0px 0px 10px 2px white");
+	document.getElementById('hr1') .style["boxShadow"] = ("0px 0px 8px 0.75px white");
+	document.getElementById('hr2') .style["boxShadow"] = ("0px 0px 8px 0.75px white");
+}
 function search() {
 	console.log("Googling \"" + box.value + "\"");
 	console.log("Encoded query: \n" + encodeURIComponent(box.value));
