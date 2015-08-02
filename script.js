6a57d81daa453f052f7585fba91b4581798b8aed
Alan Jackson <big.al.jackson9092@gmail.com>
2015-08-01 20:22:11 -0400

    aehs
    dasghyd

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 var box = document.getElementById("search_box"); 
 // this should catch most URLs, or at least the ones I would type.
 var urlPattern = new RegExp("^(https?://)?[^ ]+[.][^ ]+([.][^ ]+)*(/[^ ]+)?$");
+var isfocused = true;
 // search for text in text box
 function chgR(){
 	var searchdiv =document.getElementsByClassName("searchdiv")[0];
 	searchdiv.style["boxShadow"] = ("0px 0px 10px 2px red");
-	document.getElementById('hr1') .style["boxShadow"] = ("0px 0px 6px 0.75px red");
-	document.getElementById('hr2') .style["boxShadow"] = ("0px 0px 6px 0.75px red");
+	document.getElementById('hr1') .style["boxShadow"] = ("0px 0px 4px 0.5px red");
+	document.getElementById('hr2') .style["boxShadow"] = ("0px 0px 4px 0.5px red");
 }
 function chgW(){
+	if(isfocused === false){
+		var searchdiv =document.getElementsByClassName("searchdiv")[0];
+		searchdiv.style["boxShadow"] = ("0px 0px 10px 2px white");
+		document.getElementById('hr1') .style["boxShadow"] = ("0px 0px 4px 0.5px white");
+		document.getElementById('hr2') .style["boxShadow"] = ("0px 0px 4px 0.5px white");
+	}
+}
+function focus(){
+	isfocused=true;
 	var searchdiv =document.getElementsByClassName("searchdiv")[0];
-	searchdiv.style["boxShadow"] = ("0px 0px 10px 2px white");
-	document.getElementById('hr1') .style["boxShadow"] = ("0px 0px 6px 0.75px white");
-	document.getElementById('hr2') .style["boxShadow"] = ("0px 0px 6px 0.75px white");
+	searchdiv.style["boxShadow"] = ("0px 0px 10px 2px red");
+	document.getElementById('hr1') .style["boxShadow"] = ("0px 0px 4px 0.5px red");
+	document.getElementById('hr2') .style["boxShadow"] = ("0px 0px 4px 0.5px red");
+}
+function unfocus(){
+	isfocused=false;
 }
 function search() {
 	console.log("Googling \"" + box.value + "\"");
